a542d8a4687d5184616fad298d856298077530c6
Jimmy Colatti <jimmyrcolatti@gmail.com>
2022-06-14 21:23:34 -0400

    team CRUD complete

--- routes/team.routes.js
+++ routes/team.routes.js
@@ -12,7 +12,7 @@ router.get("/team/:userID", jwtVerify, (req, res) => {
   User.findById(req.params.userID)
     .populate("team")
     .then((userDetails) => {
-      console.log(userDetails)
+      // console.log(userDetails)
       res.status(200).json(userDetails.team)
     })
     .catch((err) => {
@@ -59,6 +59,62 @@ router.post("/team/:userID", jwtVerify, (req, res) => {
     })
     .catch((err) =>
       console.log("Error while saving a new coachee in the DB: ", err)
+    )
+})
+
+// ************************************************
+// GET COACHEE DETAILS ROUTE
+// ************************************************
+
+router.get("/team/:userID/coachee/:coacheeID", jwtVerify, (req, res) => {
+  Coachee.findById(req.params.coacheeID)
+    .then((coacheeFromDB) => {
+      res.status(200).json(coacheeFromDB)
+    })
+    .catch((err) =>
+      console.log("Error while getting a coachee details from the DB: ", err)
+    )
+})
+
+// ************************************************
+// POST ROUTE: SAVE CHANGES/EDITS MADE TO COACHEE
+// ************************************************
+
+router.post("/team/:userID/coachee/:coacheeID", jwtVerify, (req, res) => {
+  const { firstName, lastName, email } = req.body
+
+  Coachee.findByIdAndUpdate(
+    req.params.coacheeID,
+    { firstName, lastName, email },
+    { new: true }
+  )
+    .then((updatedCoacheeFromDB) => {
+      res.status(201).json(updatedCoacheeFromDB)
+    })
+    .catch((err) => {
+      console.log(
+        "Error while saving the updates in the coachee to the DB: ",
+        err
+      )
+    })
+})
+
+// ************************************************
+// DELETE COACHEE FROM DATABASE
+// ************************************************
+
+router.delete("/team/:userID/coachee/:coacheeID", jwtVerify, (req, res) => {
+  Coachee.findByIdAndDelete(req.params.coacheeID)
+    .then(() => {
+      User.findByIdAndUpdate(req.params.userID, {
+        $pullAll: { team: [req.params.coacheeID] },
+      }).then((updatedUserInfo) => {
+        res.status(200).json(updatedUserInfo)
+      })
+    })
+
+    .catch((err) =>
+      console.log("Error while deleting a coachee from the DB: ", err)
     )
 })
 
